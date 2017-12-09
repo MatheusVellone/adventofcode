f910732b47bbc01b93c8f01988ed615fb127b9fd
Matheus Vellone <matheusvellone@hotmail.com>
2017-12-09 18:51:16 -0200

    2015 day 6 both part solutions

---
+++ 2015/6/part1.js
@@ -0,0 +1,52 @@
+module.exports.tests = [
+  ['turn on 0,0 through 999,999', 1000000],
+  ['toggle 0,0 through 999,0', 1000],
+  ['turn off 499,499 through 500,500', 0],
+]
+
+const commands = {
+  'turn on': () => 1,
+  'turn off': () => 0,
+  'toggle': current => 1- current,
+}
+
+module.exports.solution = (inputs) => {
+  const grid = Array.apply(null, Array(1000)).map(() => Array.apply(null, Array(1000)).map(() => 0))
+
+  const countGrid = () => {
+    let result = 0
+
+    grid.forEach((row) => {
+      row.forEach((cell) => {
+        result += (cell || 0)
+      })
+    })
+
+    return result
+  }
+
+  inputs.split('\n')
+    .forEach((input) => {
+      const [,
+        command,
+        fromX,
+        fromY,
+        toX,
+        toY
+      ] = input.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/)
+
+      const initialX = Math.min(fromX, toX)
+      const finalX = Math.max(fromX, toX)
+      const initialY = Math.min(fromY, toY)
+      const finalY = Math.max(fromY, toY)
+      for (let row = initialY; row <= finalY; row++) {
+        for (let column = initialX; column <= finalX; column++) {
+          grid[row][column] = commands[command](grid[row][column])
+        }
+      }
+    })
+
+  return countGrid()
+}
+
+module.exports.expectedResult = 569999
---
+++ 2015/6/part2.js
@@ -0,0 +1,48 @@
+module.exports.tests = []
+
+const commands = {
+  'turn on': current => current + 1,
+  'turn off': current => current && current - 1,
+  'toggle': current => current + 2,
+}
+
+module.exports.solution = (inputs) => {
+  const grid = Array.apply(null, Array(1000)).map(() => Array.apply(null, Array(1000)).map(() => 0))
+
+  const countGrid = () => {
+    let result = 0
+
+    grid.forEach((row) => {
+      row.forEach((cell) => {
+        result += (cell || 0)
+      })
+    })
+
+    return result
+  }
+
+  inputs.split('\n')
+    .forEach((input) => {
+      const [,
+        command,
+        fromX,
+        fromY,
+        toX,
+        toY
+      ] = input.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/)
+
+      const initialX = Math.min(fromX, toX)
+      const finalX = Math.max(fromX, toX)
+      const initialY = Math.min(fromY, toY)
+      const finalY = Math.max(fromY, toY)
+      for (let row = initialY; row <= finalY; row++) {
+        for (let column = initialX; column <= finalX; column++) {
+          grid[row][column] = commands[command](grid[row][column])
+        }
+      }
+    })
+
+  return countGrid()
+}
+
+module.exports.expectedResult = 17836115
