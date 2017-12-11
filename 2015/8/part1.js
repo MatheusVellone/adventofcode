1a2e7f381a983973289615a8b40211c869096f57
Matheus Vellone <matheusvellone@hotmail.com>
2017-12-10 22:00:51 -0200

    2015 day 8 solution

--- 2015/8/part1.js
+++ 2015/8/part1.js
@@ -1,5 +1,17 @@
-module.exports.tests = []
+module.exports.tests = [
+  [`""
+"abc"
+"aaa\\"aaa"
+"\\x27"`, 12]
+]
 
-module.exports.solution = () => {
+module.exports.solution = (inputs) => {
+  let result = 0
 
+  inputs.split('\n')
+    .forEach((input) => {
+      result += input.length - eval(input).length
+    })
+
+  return result
 }
--- 2015/8/part2.js
+++ 2015/8/part2.js
@@ -1,5 +1,19 @@
-module.exports.tests = []
+module.exports.tests = [
+  [`""
+"abc"
+"aaa\\"aaa"
+"\\x27"`, 19]
+]
 
-module.exports.solution = () => {
+module.exports.solution = (inputs) => {
+  let result = 0
 
+  inputs.split('\n')
+    .forEach((input) => {
+      result += JSON.stringify(input).length - input.length
+    })
+
+  return result
 }
+
+module.exports.expectedSolution = 2074
