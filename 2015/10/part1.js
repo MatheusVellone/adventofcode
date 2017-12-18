f3342381a35c54e1ddf42df105b2c2ab08fd43c0
Matheus Vellone <matheusvellone@hotmail.com>
2017-12-17 23:02:49 -0200

    2015-10

--- 2015/10/part1.js
+++ 2015/10/part1.js
@@ -1,7 +1,39 @@
-module.exports.tests = []
+module.exports.tests = [
+  ['1', 82350],
+  ['11', 107312],
+  ['21', 139984],
+  ['1211', 182376],
+  ['111221', 237746],
+]
 
-module.exports.solution = () => {
+const runSolution = (input) => {
+  let current = input[0]
+  let result = ''
+  let count = 0
 
+  input.split('')
+    .forEach((char) => {
+      if (char === current) {
+        count += 1
+      } else {
+        result += count + current
+
+        current = char
+        count = 1
+      }
+    })
+  result += count + current
+  return result
 }
 
-module.exports.expectedResult = undefined
+module.exports.solution = (input) => {
+  let result = input
+
+  for(let i = 0; i < 40; i++) {
+    result = runSolution(result)
+  }
+
+  return result.length
+}
+
+module.exports.expectedResult = 252594
--- 2015/10/part2.js
+++ 2015/10/part2.js
@@ -1,7 +1,40 @@
-module.exports.tests = []
+module.exports.tests = [
+  ['1', 1166642],
+  ['11', 1520986],
+  ['21', 1982710],
+  ['1211', 2584304],
+  ['111221', 3369156],
+]
 
-module.exports.solution = () => {
 
+const runSolution = (input) => {
+  let current = input[0]
+  let result = ''
+  let count = 0
+
+  input.split('')
+    .forEach((char) => {
+      if (char === current) {
+        count += 1
+      } else {
+        result += count + current
+
+        current = char
+        count = 1
+      }
+    })
+  result += count + current
+  return result
 }
 
-module.exports.expectedResult = undefined
+module.exports.solution = (input) => {
+  let result = input
+
+  for(let i = 0; i < 50; i++) {
+    result = runSolution(result)
+  }
+
+  return result.length
+}
+
+module.exports.expectedResult = 3579328
