6695ff200e57113cbca0e5e701ed445ff0c693f0
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2020-11-16 23:15:39 -0300

    2016 1

--- 2016/1/part1.js
+++ 2016/1/part1.js
@@ -1,7 +1,55 @@
-module.exports.tests = []
+module.exports.tests = [
+  ['R2, L3', 5],
+  ['R2, R2, R2', 2],
+  ['R5, L5, R5, R3', 12],
+]
 
-module.exports.solution = () => {
+const turn = (current, turnDirection) => {
+  let newDirection = current
 
+  if (turnDirection === 'R') {
+    newDirection += 1
+  } else {
+    newDirection -= 1
+  }
+
+  if (newDirection === 4) {
+    newDirection = 0
+  }
+
+  if (newDirection === -1) {
+    newDirection = 3
+  }
+
+  return newDirection
+}
+
+module.exports.solution = (input) => {
+  let facing = 0
+  const position = {
+    x: 0,
+    y: 0,
+  }
+
+  input
+    .split(', ')
+    .forEach((instruction) => {
+      const [direction, ...steps] = instruction.split('')
+
+      facing = turn(facing, direction)
+
+      if (facing === 0) {
+        position.x += Number(steps.join(''))
+      } else if (facing === 2) {
+        position.x -= Number(steps.join(''))
+      } if (facing === 1) {
+        position.y += Number(steps.join(''))
+      } else if (facing === 3) {
+        position.y -= Number(steps.join(''))
+      }
+    })
+
+  return Math.abs(position.x) + Math.abs(position.y)
 }
 
 module.exports.expectedResult = undefined
--- 2016/1/part2.js
+++ 2016/1/part2.js
@@ -1,7 +1,78 @@
-module.exports.tests = []
+module.exports.tests = [
+  ['R8, R4, R4, R8', 4],
+]
 
-module.exports.solution = () => {
+const turn = (current, turnDirection) => {
+  let newDirection = current
 
+  if (turnDirection === 'R') {
+    newDirection += 1
+  } else {
+    newDirection -= 1
+  }
+
+  if (newDirection === 4) {
+    newDirection = 0
+  }
+
+  if (newDirection === -1) {
+    newDirection = 3
+  }
+
+  return newDirection
+}
+
+module.exports.solution = (input) => {
+  let facing = 0
+  const position = {
+    x: 0,
+    y: 0,
+  }
+  const visited = {}
+
+  const moveByOne = (dx, dy, negative) => {
+    const coord = dx !== 0 ? 'x' : 'y'
+
+    for (let i = 0; i < Math.abs(dx + dy); i += 1) {
+      if (firstVisitedDistance) {
+        return
+      }
+
+      position[coord] += 1 * negative
+
+      const key = position.x + '/' + position.y
+      if (visited[key]) {
+        firstVisitedDistance = Math.abs(position.x) + Math.abs(position.y)
+      }
+
+      visited[key] = true
+    }
+  }
+
+  let firstVisitedDistance = false
+
+  input
+    .split(', ')
+    .forEach((instruction) => {
+      if (firstVisitedDistance !== false) {
+        return
+      }
+      const [direction, ...steps] = instruction.split('')
+
+      facing = turn(facing, direction)
+
+      if (facing === 0) {
+        moveByOne(Number(steps.join('')), 0, 1)
+      } else if (facing === 2) {
+        moveByOne(Number(steps.join('')), 0, -1)
+      } if (facing === 1) {
+        moveByOne(0, Number(steps.join('')), 1)
+      } else if (facing === 3) {
+        moveByOne(0, Number(steps.join('')), -1)
+      }
+    })
+
+  return firstVisitedDistance
 }
 
 module.exports.expectedResult = undefined
